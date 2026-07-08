a148767c07bee2d070df165a34bd59528f47eebb
Madhavi Pawar <madhavippawar2004@gmail.com>
2026-07-08 14:45:22 +0530

    Add functionality to find and print topper from JSON

---
+++ app.py
@@ -0,0 +1,9 @@
+import json
+
+with open("students.json", "r") as f:
+    students = json.load(f)
+
+topper = max(students, key=lambda x: x["marks"])
+
+print("Topper:", topper["name"])
+print("Marks:", topper["marks"])
