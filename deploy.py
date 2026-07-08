ca4f904ef6492f36e694d963e5b9d4d9539bc743
Madhavi Pawar <madhavippawar2004@gmail.com>
2026-07-08 15:24:48 +0530

    Switch to paramiko for EC2 deployment
    Refactor deployment script to use paramiko for SSH connections and command execution.

--- deploy.py
+++ deploy.py
@@ -1,7 +1,36 @@
-import os
+import paramiko
 
-print("Starting Deployment", flush=True)
+HOST = "35.154.133.229"
+USERNAME = "ubuntu"          # Use "ubuntu" because your EC2 instance is Ubuntu
+KEY = r"C:\Users\madha\Downloads\gitKey.pem"
 
-os.system("python app.py")
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-print("Deployment Successful", flush=True)
+print("Connecting to EC2...")
+
+client.connect(
+    hostname=HOST,
+    username=USERNAME,
+    key_filename=KEY
+)
+
+commands = [
+    "cd ~/aws-python-deployment-demo && git pull",
+    "cd ~/aws-python-deployment-demo && python3 app.py"
+]
+
+for command in commands:
+    print(f"Running: {command}")
+
+    stdin, stdout, stderr = client.exec_command(command)
+
+    print(stdout.read().decode())
+
+    error = stderr.read().decode()
+    if error:
+        print(error)
+
+client.close()
+
+print("Deployment Completed")
